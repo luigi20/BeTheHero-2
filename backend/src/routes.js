e0b4766baa03881daed124c2537d15f77ecb75ec
Luis <luisopentec@gmail.com>
2021-11-04 12:43:43 -0300

    change in route name and adoption of token for authentication

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,22 +3,36 @@ const crypto = require('crypto');
 const routes = express.Router();
 const bcrypt = require('bcryptjs');
 const connection = require('./database/connection');
+const authconfig = require('./config/auth.json');
+const jwt = require('jsonwebtoken');
+function generateToken(params = {}) {
+    const token = jwt.sign(params, authconfig.secret, {
+        expiresIn: 86400,
+    });
+    return token;
+}
 
-routes.post('/ongs', async (req, res) => {
-    const { name, email, whatsapp, city, uf, password } = req.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-    const hash = await bcrypt.hash(password, 10);
-    this.password = hash;
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-        password
-    })
-    return res.send();
+routes.post('/register_ongs', async (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body;
+    try {
+        if (await connection('ongs').where('email', email).first()) {
+            return res.status(400).send({ Error: "Ong Already Exists" })
+        }
+        const id = crypto.randomBytes(4).toString('HEX');
+        const password = await bcrypt.hash(req.body.password, 10);
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+            password
+        })
+        return res.send({ token: generateToken({ idToken: id }) });
+    } catch (error) {
+        return res.status(400).send({ Error: "Registration Failed" })
+    }
 });
 
 module.exports = routes;
