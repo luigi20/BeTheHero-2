fe579e8ef83699ea632888ef112d8d6e444eadea
Luis <luisopentec@gmail.com>
2021-11-17 10:56:13 -0300

    creation of the route and the profile page, in addition to the newincident route and the css of the pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function RoutesApp() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" exact element={<Logon />} />
-                <Route path="/register" exact element={<Register />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/incidents/new" element={<NewIncident />} />
             </Routes>
         </BrowserRouter>
     );
