55e3dd664db3701c74d26815034d20c1d6b8ead3
Luis <luisopentec@gmail.com>
2021-11-22 14:33:43 -0300

    refactoring ONG registration screen

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import './styles.css';
 import { Link, useNavigate } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
+import { useForm } from 'react-hook-form';
 
 export default function Register() {
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [uf, setUf] = useState();
-    const [whatsapp, setWhatsapp] = useState();
-    const [city, setCity] = useState();
+    /*  const [name, setName] = useState();
+      const [email, setEmail] = useState();
+      const [password, setPassword] = useState();
+      const [uf, setUf] = useState();
+      const [whatsapp, setWhatsapp] = useState();
+      const [city, setCity] = useState();*/
 
     const navigate = useNavigate();
 
-    async function handleRegister(e) {
+    const { register, handleSubmit, errors, reset } = useForm({
+        defaultValues: { name: "", email: "", password: "", uf: "", whatsapp: "", city: "" },
+    });
+
+
+
+    async function handleRegister({ name, email, password, uf, whatsapp, city }, e) {
         e.preventDefault();
         const dataOng = {
             name,
@@ -49,31 +56,28 @@ export default function Register() {
                         Não tenho cadastro
                     </Link>
                 </section>
-                <form onSubmit={handleRegister}>
+                <form onSubmit={handleSubmit(handleRegister)}>
                     <input placeholder="Nome da ONG"
-                        value={name}
-                        onChange={e => setName(e.target.value)}
+                        {...register('name', { required: true })}
                     />
                     <input type="email" placeholder="E-mail"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
+                        {...register('email', { required: true })}
                     />
                     <input type="password" placeholder="Senha"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        {...register('password', { required: true })}
                     />
                     <input placeholder="Whatsapp"
-                        value={whatsapp}
-                        onChange={e => setWhatsapp(e.target.value)}
+                        {...register('whatsapp', { required: true })}
                     />
                     <div className="input-group">
-                        <input placeholder="Cidade"
-                            value={city}
-                            onChange={e => setCity(e.target.value)}
-                        />
-                        <input placeholder="UF" style={{ width: 80 }}
-                            value={uf}
-                            onChange={e => setUf(e.target.value)}
+                        <select placeholder="UF" >
+                            <option selected value="UF">UF</option>
+                            <option value="laranja">Laranja</option>
+                            <option value="limao">Limão</option>
+                            <option value="manga">Manga</option>
+                        </select>
+                        <input placeholder="Cidade" style={{ width: 80 }}
+                            {...register('cidade', { required: true })}
                         />
                     </div>
                     <button className="button" type="submit">Cadastrar</button>
